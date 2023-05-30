a967cf30911c376949693c77f8fb1f3d6951a5f5
Abdullah ALLALI <contact@allali.me>
2023-05-30 10:52:44 +0100

    Update constants.go

--- src/config/constants.go
+++ src/config/constants.go
@@ -8,23 +8,26 @@ var Infos = struct {
 	Description string
 	Short       string
 	Usage       string
+	VCheckUrl   string
 }{
-	Version: "0.0.1",
-	GitRep:  "https://github.com/aallali/deepeye",
-	Short:   "CLI for fast/efficient searching through file.",
-	Usage:   "deepeye <filename> flags",
+	Version:   "0.0.1",
+	GitRep:    "https://github.com/aallali/deepeye/",
+	Short:     "CLI for fast/efficient searching queries through files.",
+	Usage:     "deepeye <filename> flags",
+	VCheckUrl: "https://github.com/aallali/deepeye/raw/main/version.txt",
 }
 
 func init() {
 
 	Infos.Description = fmt.Sprintf(`
 DeepEye: a CLI that will allow you to run advanced search queries
-	through multile text files, while having customized + detailed output.
+	through multipe text files, while having customized + detailed output.
 	The "deepeye" program was mainly focused on helping terminal users, 
 	to quickly search in files in both plain text and regex queries.
 		
-Willing to contribute ? : "%s"
+Willing to contribute? : "%s"
 
 Author: Abdellah Allali <hi@allali.me>
-Birth: 24/05/2023`, Infos.GitRep)
+Birth: 24/05/2023
+First release: 30/05/2023`, Infos.GitRep)
 }
